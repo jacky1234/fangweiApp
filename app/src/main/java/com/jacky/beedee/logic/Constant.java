d54608c9c9b29f360268bde45685e7770a254d8a
jacky1234 <jackdoson2011@gmail.com>
2018-12-06 15:49:43 +0800

    百度统计

--- app/src/main/java/com/jacky/beedee/logic/Constant.java
+++ app/src/main/java/com/jacky/beedee/logic/Constant.java
@@ -4,6 +4,7 @@ public final class Constant {
     public static final String WX_APP_ID = "wx7266547dcee7b1b6";
     public static final String QQ_APP_ID = "1107909602";
     public static final String WB_APP_ID = "2096526831";
+    public static final String BAIDU_MTJ_APPID = "3ec3ba5e8d";
 
     public static final String WB_CALLBACK_URL = "https://api.beedeemade.com/weibo/authorize/callback";
 }
--- app/src/main/java/com/jacky/beedee/support/util/AndroidUtil.java
+++ app/src/main/java/com/jacky/beedee/support/util/AndroidUtil.java
@@ -8,7 +8,11 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.provider.MediaStore;
@@ -21,6 +25,12 @@ import android.widget.Toast;
 import com.jacky.beedee.support.Starter;
 
 import org.jetbrains.annotations.Nullable;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 
 /**
@@ -138,5 +148,67 @@ public class AndroidUtil {
     public static void setMaxLengthForEditText(EditText mEditText, int maxLength) {
         InputFilter[] filters = {new InputFilter.LengthFilter(maxLength)};
         mEditText.setFilters(filters);
+    }
+
+    public static String getIpAddress(Context context) {
+        NetworkInfo info = ((ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        if (info != null && info.isConnected()) {
+            // 3/4g网络
+            if (info.getType() == ConnectivityManager.TYPE_MOBILE) {
+                try {
+                    for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                        NetworkInterface intf = en.nextElement();
+                        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                            InetAddress inetAddress = enumIpAddr.nextElement();
+                            if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                                return inetAddress.getHostAddress();
+                            }
+                        }
+                    }
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }
+
+            } else if (info.getType() == ConnectivityManager.TYPE_WIFI) {
+                //  wifi网络
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                String ipAddress = intIP2StringIP(wifiInfo.getIpAddress());
+                return ipAddress;
+            } else if (info.getType() == ConnectivityManager.TYPE_ETHERNET) {
+                // 有限网络
+                return getLocalIp();
+            }
+        }
+        return null;
+    }
+
+    private static String intIP2StringIP(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                (ip >> 24 & 0xFF);
+    }
+
+    // 获取有限网IP
+    private static String getLocalIp() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()
+                            && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+
+        }
+        return "0.0.0.0";
     }
 }
